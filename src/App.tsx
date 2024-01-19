ece87df838de631ef7515c248a6c91372ab4f0c5
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-19 18:10:48 +0530

    Adding the final project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { useAppDispatch, useAppSelector } from './app/hooks'
+import { increment } from './features/counter/counterSlice'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const count = useAppSelector((state) => state.counter.value)
+
+  const dispatch = useAppDispatch()
+
+  const handleClick = () => {
+    dispatch(increment())
+  }
 
   return (
     <>
@@ -18,7 +26,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={handleClick}>
           count is {count}
         </button>
         <p>
